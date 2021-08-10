fed5e1b13ca8563102770ffc49f5280d5022d8ac
Prof. Márcio Assis <assismiranda@gmail.com>
2021-08-10 14:10:21 -0300

    exemplos JS

---
+++ Calculadora JS/calculaIMC.js
@@ -0,0 +1,11 @@
+function calculaIMC () {
+    var formulario = document.getElementById("formulario");
+
+    var kilos = +formulario.kilos.value;
+    var metros = +formulario.metros.value;
+    var centimetros = +formulario.centimetros.value;
+
+    var altura = (metros * 100 + centimetros) / 100;
+    var imc = kilos / (altura * altura);
+    formulario.imc.value = imc.toFixed(2);
+}
